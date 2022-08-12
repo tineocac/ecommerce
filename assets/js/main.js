cfbc5a266ed0ba381a794ca4e0588ea406b45cdd
SaraChavezCh <sarachavez856@gmail.com>
2022-08-12 17:41:53 -0500

    [ADD] codigo maestra

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,43 @@
-const loader = document.getElementById("loader")
 
 document.addEventListener("DOMContentLoaded", () =>{
     load()
 })
-
+/* =========LOADER==========*/
+const loader = document.getElementById("loader")
 function load (){
     setTimeout( () =>{
         loader.classList.add("hide")
     }, 3000)
 }
+
+/* ======DARK MODE===*/
+const themeButton = document.getElementById("theme-button")
+
+themeButton.addEventListener("click", () =>{
+    document.body.classList.toggle("dark-theme")
+      if(themeButton.classList.contains("bx-moon")){
+        themeButton.classList.replace("bx-moon","bx-sun" )}
+    else{
+        themeButton.classList.replace("bx-sun","bx-moon" )
+    }
+})
+
+/*=========CARRITO========*/
+const cartOpen = document.getElementById("cart-shop")
+const cartClose = document.getElementById("close-shop")
+const cartContainer = document.getElementById("cart--container")
+
+cartOpen.addEventListener("click",() =>{
+    cartContainer.classList.remove("hide")
+})
+cartClose.addEventListener("click",()=>{
+    cartContainer.classList.add("hide")
+})
+
+/*=========SCROLL========*/
+const header=document.getElementById("header")
+window.addEventListener("scroll",() =>{
+    if(window.screenY >= 50){
+header.classList.add("scroll-header")}
+else{ header-classList.remove("scroll-header")}
+})
