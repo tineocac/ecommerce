f9fe2382f50e85b4a6b1a4da6c9c8e204f335588
SaraChavezCh <sarachavez856@gmail.com>
2022-08-13 19:58:47 -0500

    [ADD] html and css styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,7 @@ cartClose.addEventListener("click",()=>{
     cartContainer.classList.add("hide")
 })
 
+
 /*=========GRID ABRIR Y CERRAR========*/
 const gridOpen = document.getElementById("nav-toggle")
 const gridClose = document.getElementById("close-grid")
@@ -48,8 +49,48 @@ gridClose.addEventListener("click",()=>{
 
 /*=========SCROLL========*/
 const header=document.getElementById("header")
+const nav = document.getElementById("nav")
+
 window.addEventListener("scroll",() =>{
     if(window.screenY >= 50){
 header.classList.add("scroll-header")}
-else{ header-classList.remove("scroll-header")}
+else{ header.classList.remove("scroll-header")}
 })
+
+/*=======scroll boton===========*/
+
+// const discover = document.getElementById("discover")
+// discover.addEventListener("click",() =>
+// {window.scrollTo({
+//     top: 100,
+//     left: 100,
+//     behavior: 'smooth'
+//   });})
+
+/*=============ITEMS=========== */
+const items = [
+    {
+      id: 1,
+      name: 'Hoodies',
+      price: 14.00,
+      image: 'assets/img/featured1.png',
+      category: 'hoodies',
+      quantity: 10
+    },
+    {
+      id: 2,
+      name: 'Shirts',
+      price: 24.00,
+      image: 'assets/img/featured2.png',
+      category: 'shirts',
+      quantity: 15
+    },
+    {
+      id: 3,
+      name: 'Sweatshirts',
+      price: 24.00,
+      image: 'assets/img/featured3.png',
+      category: 'sweatshirts',
+      quantity: 20
+    }
+  ]
