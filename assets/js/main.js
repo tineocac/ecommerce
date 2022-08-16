8ee0fb05008940ce03a00dc4296eb793b82e0ea5
SaraChavezCh <sarachavez856@gmail.com>
2022-08-16 17:26:19 -0500

    [ADD] carrito de compras vacio

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,14 +57,33 @@ const cartOpen = document.getElementById("cart-shop")
 const cartClose = document.getElementById("close-shop")
 const cartContainer = document.getElementById("cart--container")
 
+let shoppingItems = document.getElementById("cart-bag")
+let counter = document.getElementById("cart-counter")
+
 cartOpen.addEventListener("click",() =>{
     cartContainer.classList.remove("hide")
 })
 cartClose.addEventListener("click",()=>{
     cartContainer.classList.add("hide")
-})
+  })
 
+  
+  if(counter === 0){
 
+  shoppingItems.innerHTML=`
+   <div class = "shopping-container-img">
+       <img class = "shopping-empty" src="./assets/images/empty-cart.png">
+       <h2 class="shopping-tittle">Your cart is empty</h2>
+       <p class="shopping-paragraph">You can add items to your cart by clicking on the "<i class='bx bx-plus'></i>" button on the product page.</p>
+    </div> 
+    `
+  }
+  else{
+    shoppingItems =` /*elementos del carrito*/
+    ` 
+  }
+
+  
 /*=========GRID ABRIR Y CERRAR========*/
 const gridOpen = document.getElementById("nav-toggle")
 const gridClose = document.getElementById("close-grid")
@@ -113,10 +132,10 @@ const productContainer = document.getElementById("products-list" )
         <div class="product-card" id="${product.id}">
             <div class="container-card-1">
             <img class ="showProduct-img" src=${product.image} alt="">
-            <button class="btn-add">+</button>
+            <button class="btn-add"><i class='bx bx-plus'></i></button>
             </div>
             <div class="container-card-2">
-            <span class="price-card">$${product.price}.00</span>
+            <span class="price-card">${numberToCurrency(product.price)}</span>
             <small class="stock-card">Stock ${ product.quantity}</small>
             <span class="name-card">${product.name}</span>
             </div>
@@ -126,13 +145,21 @@ const productContainer = document.getElementById("products-list" )
     productContainer.innerHTML = fragment
 }
 
-let valor = 0
-
-function aumentarContador(){
-  let contador = document.querySelector(".counter");
-  valor++;
-  contador.textContent = valor;
+function numberToCurrency (value) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(value)
 }
 
-const btnAdd = document.querySelector(".btn-add")
+// let valor = 0
 
+// function aumentarContador(){
+//   let contador = document.querySelector(".counter");
+//   valor++;
+//   contador.textContent = valor;
+// }
+
+// const btnAdd = document.querySelector(".btn-add")
+
+let contador = document.get
