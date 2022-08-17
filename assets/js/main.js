d3228375ff932cf3be085bbe1d3f0d080ec33ad9
SaraChavezCh <sarachavez856@gmail.com>
2022-08-17 15:30:33 -0500

    mostrar en el carrito de compras en proceso

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,74 +52,61 @@ cartClose.addEventListener("click",()=>{
 })
 
 /*===COUNTER BAG===*/
+
 let counter = document.getElementById("cart-counter")
 let cantidadProductos = 0;
 // counter = cantidadProductos 78
+let shoppingItems = document.getElementById("cart-bag")
 
 function cartFunctionality(){
   const btnsAdd = document.querySelectorAll(".btn-add")
-  console.log(btnsAdd);
+  // console.log(btnsAdd); // [Todos los botones]
   const shoppingBag = []
+  console.log(shoppingBag);
   
   btnsAdd.forEach (button => {
     button.addEventListener("click", e =>{
-      console.log(e);
+      // console.log(e);
       const id = parseInt(e.target.parentElement.parentElement.id)
-      console.log(id);
+      // console.log(id);
       const selectProduct = items.find(item => item.id === id)
-
+        // console.log(selectProduct);
       cantidadProductos++
       counter.textContent = cantidadProductos
       shoppingBag.push(selectProduct)
+
+      if(shoppingBag === 0){
+
+        shoppingItems.innerHTML=`
+        <div class = "shopping-container-img" id="bag-empty">
+              <img class = "shopping-empty" src="./assets/images/empty-cart.png">
+              <h2 class="shopping-tittle">Your cart is empty</h2>
+              <p class="shopping-paragraph">You can add items to your cart by clicking on the "<i class='bx bx-plus'></i>" button on the product page.</p>
+          </div> 
+          `
+      }
+      else{shoppingItems.innerHTML=`
+      <div class="product-card" >
+      <div class="container-card-1" id="${product.id}">
+          <img class ="showProduct-img" src=${product.image} alt="">
+          <button class="btn-add"><i class='bx bx-plus'></i></button>
+          </div>
+          <div class="container-card-2">
+          <span class="price-card">${numberToCurrency(product.price)}</span>
+          <small class="stock-card">Stock ${ product.quantity}</small>
+          <span class="name-card">${product.name}</span>
+          </div>
+      </div>
+      `
+      }
     })
   })
 }
 
-/*======== MOSTRAR ELEMENTOS EN LA BAG =====*/
-// function showProducts (productsArray){
-//   let cart = document.getElementById("cart")
-//   let fragmentHTML = ``
-//   productsArray.forEach(product =>{
-//     fragmentHTML += `
-//     <div class="product-card" >
-//           <div class="container-card-1" id="${product.id}">
-//               <img class ="showProduct-img" src=${product.image} alt="">
-//               <button class="btn-add"><i class='bx bx-plus'></i></button>
-//               </div>
-//               <div class="container-card-2">
-//               <span class="price-card">${numberToCurrency(product.price)}</span>
-//               <small class="stock-card">Stock ${ product.quantity}</small>
-//               <span class="name-card">${product.name}</span>
-//               </div>
-//           </div>
-//           <span class="cart--detalles__subtotal">subtotal: $${cartProduct.SubPrice}</span>
-//           <div class="cart__amount">
-//                       <div class="cart__amount-content">
-//                       <span class="cart__amount__box">
-//                       <i class="bx bx-minus"></i>
-//                       </span>
-//                       <span class="cart__amount-number">${cartProduct.cantidad} unidades</span>
-//                       <span class="cart__amount__box">
-//                       <i class="bx bx-plus"></i>
-//                       </span>
-//                       </div>
-//                       <i class="bx bx-trash-alt cart__amount-trash"></i>
-//           </div>`
-//   }) 
-//   cart.innerHTML = fragmentHTML
-// }
-
-
 /*=======CART EMPTY=======*/
-let shoppingItems = document.getElementById("cart-bag")
 
-shoppingItems.innerHTML=`
-<div class = "shopping-container-img" id="bag-empty">
-      <img class = "shopping-empty" src="./assets/images/empty-cart.png">
-      <h2 class="shopping-tittle">Your cart is empty</h2>
-      <p class="shopping-paragraph">You can add items to your cart by clicking on the "<i class='bx bx-plus'></i>" button on the product page.</p>
-  </div> 
-  `
+
+
 
 
 
